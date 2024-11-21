89c4ece6c49ded9395fc50c5dc58f222316745c0
Ryan DECIAN <ryan.decian.propgmail.com>
2024-11-21 12:47:00 +0100

    Intégration du composant footer qui reste cependant a développer

---
+++ client/src/components/FooterRoot/FooterRoot.tsx
@@ -0,0 +1,11 @@
+import "./FooterRoot.css";
+
+function FooterRoot() {
+  return (
+    <section className="FooterRoot">
+      <h4>© GenkiAnime by Ryan DECIAN / 2024</h4>
+    </section>
+  );
+}
+
+export default FooterRoot;
